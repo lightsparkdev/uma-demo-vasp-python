43f8ed466779ee48357250deb59c337c80e5ad40
Zhen Lu <zhenlu@lightspark.com>
2024-09-03 18:47:26 -0700

    Missing file

---
+++ uma_vasp/demo/demo_uma_request_storage.py
@@ -0,0 +1,20 @@
+from typing import Dict
+import dataclasses
+
+
+@dataclasses.dataclass
+class RequestStorage:
+    def __init__(self):
+        self._cache = {}
+
+    def save_request(self, request_id: str, request: Dict):
+        self._cache[request_id] = request
+
+    def get_request(self, request_id: str) -> Dict:
+        return self._cache.get(request_id)
+
+    def delete_request(self, request_id: str):
+        self._cache.pop(request_id, None)
+
+    def get_requests(self) -> Dict:
+        return self._cache
